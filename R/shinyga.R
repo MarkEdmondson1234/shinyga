d07f30091d9a9f6f04b1094c0692a8a1f00abb46
github@markedmondson.me <mark@FamilyComputer.local>
2015-03-08 17:44:52 +0100

    awesomeAuthMacro

---
+++ R/shinyga.R
@@ -0,0 +1,21 @@
+#' shinyga: Easier Google Analytics Dashboards in Shiny.
+#'
+#' This package adds functions to be used within a Shiny project, that quickstart
+#' importing and displaying Google Analytics data.
+#' 
+#' @section Authentication functions:
+#' \code{\link{renderAuthDropdownRow}}
+#' 
+#' @section Message Functions:
+#' \code{\link{initMessageData}}
+#' \code{\link{addMessageData}}
+#'
+#' @section Fetch Data Functions:
+#' \code{\link{rollupGA}}
+#' 
+#' @section Macro Functions:
+#' \code{\link{metricSelect}}
+#' 
+#' @docType package
+#' @name shinyga
+NULL
