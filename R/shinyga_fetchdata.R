aa928fe7bd9cdec050f7f52d84498ff341227be7
github@markedmondson.me <mark@FamilyComputer.local>
2015-03-22 13:52:06 +0100

    installation improvements from paveljasek feedback

--- R/shinyga_fetchdata.R
+++ R/shinyga_fetchdata.R
@@ -197,6 +197,8 @@ shinygaGetSegments = function(token, start=1, max=1000) {
 #'}
 processManagementData = function(url, keep) {
   
+  options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+  
   ga.json <- RJSONIO::fromJSON(RCurl::getURL(url))
   
   if (is.null(ga.json)) { stop('data fetching did not output correct format'); }
@@ -422,6 +424,11 @@ MEgetData = function(ids,
                      return.url = FALSE, rbr = FALSE, envir = .GlobalEnv,
                      token) {
   
+  options(RCurlOptions = list(verbose = FALSE, 
+                              capath = system.file("CurlSSL", "cacert.pem", package = "RCurl"),
+                              cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"),
+                              ssl.verifypeer = FALSE))
+  
   if (missing(ids)) { stop('please enter a profile id') }
   
   if (missing(batch) || batch == FALSE) {
@@ -484,9 +491,7 @@ MEgetData = function(ids,
   # get data and convert from json to list-format
   # thanks to Schaun Wheeler this will not provoke the weird SSL-bug
   # switched to use httr
-  options(RCurlOptions = list(verbose = FALSE, 
-                              capath = system.file("CurlSSL", "cacert.pem", package = "RCurl"), 
-                              ssl.verifypeer = FALSE))
+
   request <- httr::GET(url);
   ga.data <- httr::content(request);
   
