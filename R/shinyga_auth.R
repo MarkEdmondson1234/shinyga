a21af0b5f0b4efbca8c6361c97f17e40420ae04e
github@markedmondson.me <mark@FamilyComputer.local>
2015-04-02 15:04:40 +0200

    hardcode client demo info

--- R/shinyga_auth.R
+++ R/shinyga_auth.R
@@ -256,8 +256,8 @@ appURL <- function(session){
 shinyga_get_google_token <- function() {
 
   token <- shinygaGetToken(code = securityCode,
-                           client.id     = CLIENT_ID,
-                           client.secret = CLIENT_SECRET,
+                           client.id     = "473754740135-no6o8ckigfe00jb1sbvpsfm4gcqbjk9r.apps.googleusercontent.com",
+                           client.secret = "NOEkEfAR3ANQB6AKSZKFIeWj",
                            redirect.uri  = appURL(session))
 }
 
