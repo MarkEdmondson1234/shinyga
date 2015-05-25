b70c992c7208189837228a3394819af56f5e7f61
github@markedmondson.me <mark@FamilyComputer.local>
2015-05-25 12:45:17 +0200

    adding custom Google scopes to doAuthMacro

--- R/shinyga_auth.R
+++ R/shinyga_auth.R
@@ -147,8 +147,7 @@ shinygaGetTokenURL <-
            redirect.uri  = CLIENT_URL,
            scope         = c("https://www.googleapis.com/auth/analytics",
                              "https://www.googleapis.com/auth/analytics.readonly",
-                             "https://www.googleapis.com/auth/analytics.manage.users.readonly",
-                             "https://www.googleapis.com/auth/analytics.manage.users")) {
+                             "https://www.googleapis.com/auth/analytics.manage.users.readonly")) {
     
   scopeEnc <- sapply(scope, URLencode, reserved=TRUE)
   scopeEnc <- paste(scopeEnc, sep='', collapse='+')
